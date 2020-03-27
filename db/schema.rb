6075f2bdc470ea906a0fe168a0ee340f2f663194
Matheus Cunha <mathcunha@gmail.com>
2020-03-27 20:40:33 +0000

    rails g scaffold Person name:string email:string photo:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_27_203949) do
+ActiveRecord::Schema.define(version: 2020_03_27_204018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2020_03_27_203949) do
     t.string "name"
     t.string "link"
     t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "photo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
